15963e876cff3a858f194127acfb77f8e5941208
Soumya Bhatnagar <soumyabhtngr@gmail.com>
2021-11-13 18:53:16 +0530

    deleted comments

--- src/posts-store.ts
+++ src/posts-store.ts
@@ -1,20 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
 
-// const _posts = [
-//     {
-//         id: 1,
-//         title: "My first blog post",
-//         text: "Hello world! This is my first blog post on my new Cloudflare Workers + Pages blog.",
-//         published_at: new Date("2020-10-23")
-//     },
-//     {
-//         id: 2,
-//         title: "Updating my blog",
-//         text: "It's my second blog post! I'm still writing and publishing using Cloudflare Workers + Pages :)",
-//         published_at: new Date("2020-10-26")
-//     }
-// ]
-
 export default class PostsStore {
     async getAllPosts() {
         // @ts-ignore
